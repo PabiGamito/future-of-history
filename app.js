00542dcf33dda07c9927dcd5b6b564a1ba4b7172
Pablo Gamito <pablogamito@gmail.com>
2016-07-15 17:59:07 +0200

    Search Query Database Key auto increment
    Search queries now save in database with auto incrementing keys +
    removed unnecessary semicolons.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Save Google Queries to Database
 if(window.location.href.match(/www.google.[a-z\.]+\/search/g) || window.location.href.match(/www.google.[a-z\.]+\/.+#q=/g)){
   //Open connection to "future" database
-  var openRequest = indexedDB.open("future",1);
+  var openRequest = indexedDB.open("future",1)
   openRequest.onsuccess = function(e) {
     db = e.target.result;
-    var transaction = db.transaction(["searches"],"readwrite");
-    var store = transaction.objectStore("searches");
-    
+    var transaction = db.transaction(["searches"],"readwrite")
+    var store = transaction.objectStore("searches")
+
     var query = window.location.search.substr(1).split(/[\#\&]+/)
     var params = {}
     query.forEach(function(q){
@@ -14,24 +14,24 @@ if(window.location.href.match(/www.google.[a-z\.]+\/search/g) || window.location
       params[qs[0]] = qs[1]
     })
     //params.q => search query
-    store.add({query:params.q, ts: new Date()},1)
+    store.add({query:params.q, ts: new Date()})
   }
   openRequest.onupgradeneeded = function(e) {
     var thisDB = e.target.result
     if(!thisDB.objectStoreNames.contains("searches")) {
-	thisDB.createObjectStore("searches")
+	     thisDB.createObjectStore("searches", { autoIncrement : true })
     }
   }
   openRequest.onerror = function(e) {
-    console.log("Database Error: " + e.target.errorCode);
-    console.log("Permission to create a database might not be enabled.");
+    console.log("Database Error: " + e.target.errorCode)
+    console.log("Permission to create a database might not be enabled.")
   }
   var as = document.querySelectorAll("a")
   for(var i = 0; i < as.length; i++){
     var a = as[i]
     a.addEventListener("click", function(){
-      console.log("you clicked", this.getAttribute('data-href'));
-      console.log("with the page title", this.innerHTML);
+      console.log("you clicked", this.getAttribute('data-href'))
+      console.log("with the page title", this.innerHTML)
     })
   }
 }
