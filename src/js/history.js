27e2bd6b56c11bdaaba998ad3e7392364020db4b
Pablo Gamito <pablogamito@gmail.com>
2016-07-17 00:41:17 +0200

    Solved merge (normally)

--- src/js/history.js
+++ src/js/history.js
@@ -1,4 +1,18 @@
-var SearchesComponent = {
+requestSearchesFromIndexedDB()
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.for == "history") {
+      if (request.searches){
+        // Add loaded searches to page (default: past day searches)
+        displaySearches(request)
+      }
+    }
+  }
+)
+
+// README: Add the following code back in if needed
+/*var SearchesComponent = {
   template: '#searches',
   data: function(){
     return {
@@ -23,7 +37,7 @@ var SearchesComponent = {
         }
 
         _self.searches = items;
-      
+
       });
 
     },
@@ -31,9 +45,7 @@ var SearchesComponent = {
       return moment.unix(ts/1000).calendar()
     }
   }
-}
-
-
+}*/
 
 
 new Vue({
@@ -61,18 +73,35 @@ new Vue({
   })
 
 
-
-function fetchSearchesFromIndexDB(offset,limit,callback){
+function requestSearchesFromIndexedDB(){
   chrome.runtime.sendMessage(
-    {for: "background", action: "get", get: "searches"},
+    {for: "background", database: "get", get: "searches"},
     function(response) {
-      if (response.searches){
-          callback(null,response.searches);
-          return;
-      }
-
-      callback({error: 'failed to fetch Searches'});
-
+      // Can't use response returns as undefined instead uses onMessage to receive data
     }
   )
+}
+
+function displaySearches(request){
+  request.searches.reverse().forEach(function(searchData) {
+    var searchQuery = searchData.query
+    var formattedTime = moment.unix(searchData.ts/1000).calendar()
+    var link = searchData.href
+    var links = searchData.openedLinks
+    // TODO: add to page based on how html is coded
+    $("body").append("<p>"+formattedTime+": <a href='"+link+"'>"+searchQuery+"</a></p>")
+    links.forEach(function(linkData){
+      var href = linkData.link
+      var title = linkData.title
+      // TODO: add to page based on how html is coded
+      // TODO: "Merge" same links together and add small badge with number of times opened next to it if opened more than once.
+      $("body").append("<a href='"+href+"'>"+title+"</a><br>")
+    })
+    console.log(searchData)
+  })
+}
+
+function addSearches(){
+  // TODO: Make sure it doesn't add duplicates and does not skip some
+  // Runs on load more request from user
 }
